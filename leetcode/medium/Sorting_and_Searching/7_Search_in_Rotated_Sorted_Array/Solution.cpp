f0c40f787f279e1d2de4df01fbb15a2ab28e87c2
Seokchan An <ahnsc93@gmail.com>
2018-03-26 20:21:09 +0900

    asdf

---
+++ leetcode/medium/Sorting_and_Searching/7_Search_in_Rotated_Sorted_Array/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            
+            if (nums[mid] > nums[right]) { // mid is on the left part.
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < nums[left]) { // mid is on the right part.
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else { // there is no rotation.
+                if (target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
